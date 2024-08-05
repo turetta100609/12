a72c74f2a18ab0003b7b3c0eee5e4e37ba99c3e3
SOPHIA TURETTA <sophia.3989@aluno.pr.senac.br>
2024-08-05 12:30:08 -0300

    first commit

---
+++ assets/img/_mapear-rede (2).bat
@@ -0,0 +1,2 @@
+net use * /delete /y
+net use z: \\172.17.34.253\202400001 /USER:202400001 uept34#senac /persistent:yes
---
+++ src/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('.navbar').addClass('scrolled');
+        } else {
+            $('.navbar').removeClass('scrolled');
+        }
+    });
+});
